8081adc07c730598a6856bc251a30d0f463fc810
Alexander Holovin <dvjmixxxer@gmail.com>
2016-10-31 22:23:33 +0300

    Add to list method example

--- todoApp/todo-list/todo-list.component.js
+++ todoApp/todo-list/todo-list.component.js
@@ -1,34 +1,56 @@
 (function () {
+
   angular
     .module('todoApp')
     .component('todoList', {
-      template: '<todo-list-row ng-repeat="item in vm.list" row="item"></todo-list-row>',
+      template: '<todo-list-row ng-repeat="item in vm.list" row="item"></todo-list-row><a ng-click="vm.add()" qqq="vm">[add random]</a>',
       controller: TodoListController,
       controllerAs: 'vm'
     });
 
   TodoListController.$inject =  ['networkService'];
-
   function TodoListController(networkService) {
     var vm = this;
+    vm.init = init;
+    vm.hey = hey;
+    vm.add = add;
 
-    networkService.getData().then(function (res) {
+    init();
 
-      if (!res['todoList']) {
-        throw new Error('Empty file!');
-      }
-
-      console.log(vm.list);
-      vm.list = res['todoList'];
-
-    }).catch(function (err) {
-      console.log(err);
-    });
+    function add() {
+      item = {
+        'name': Math.random() % 10
+      };
 
 
-    this.$onInit = function () {
-      console.log('init!');
-    };
+      vm.list.push(item);
+    }
+
+    function init() {
+      networkService.getData().then(function (res) {
+
+        if (!res['todoList']) {
+          throw new Error('Empty file!');
+        }
+
+        console.log(vm.list);
+        vm.list = res['todoList'];
+
+        for (var i = 0; i < 2; i++) {
+          vm.list[i].name += Math.random() % 10;
+        }
+
+      }).catch(function (err) {
+        console.log(err);
+      });
+    }
+
+
+    function hey() {
+      console.log("hop!");
+    }
+
   }
 
 })();
+
