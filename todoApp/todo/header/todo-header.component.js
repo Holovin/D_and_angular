f02ed5d620c49b872d14193ba8a1a8988d2d5e12
Alexander Holovin <dvjmixxxer@gmail.com>
2016-11-01 15:14:48 +0300

    Header component added

---
+++ todoApp/todo/header/todo-header.component.js
@@ -0,0 +1,49 @@
+(function () {
+
+  angular
+    .module('todoApp')
+    .component('todoHeader', {
+      templateUrl: './todo/header/todo-header.template.html',
+      controller: TodoFooterController,
+      controllerAs: 'vm',
+      require: {
+        parent: '^todoList'
+      }
+    });
+
+  function TodoFooterController() {
+    var vm = this;
+
+    vm.sortItems = [
+      {
+        sortField: null,
+        displayName: 'No sort '
+      }, {
+        sortField: 'name',
+        displayName: 'Name'
+      }
+    ];
+
+    vm.$onInit = init;
+    vm.sortList = sortList;
+    vm.sortReverse = sortReverse;
+
+    function init() {
+      // ng-init can this too
+      vm.sortSelected = vm.sortItems[0];
+    }
+
+    function sortList(propertyName) {
+      console.log(propertyName);
+
+      vm.parent.reverseFlag = (propertyName === vm.parent.propertyName) ? !vm.parent.reverseFlag : false;
+      vm.parent.propertyName = propertyName;
+    }
+
+    function sortReverse() {
+      vm.parent.reverseFlag = !vm.parent.reverseFlag;
+    }
+
+  }
+
+})();
