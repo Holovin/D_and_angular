b726c5a9d683a52ddad7cca976322536bec1e9d1
Alexander Holovin <dvjmixxxer@gmail.com>
2016-11-02 04:08:03 +0300

    Controls component update

--- todoApp/todo/controls/todo-controls.component.js
+++ todoApp/todo/controls/todo-controls.component.js
@@ -5,15 +5,10 @@
     .module('todoApp.ui')
     .component('todoControls', {
       templateUrl: './todo/controls/todo-controls.template.html',
-      controller: TodoFooterController,
       controllerAs: 'vm',
       require: {
         parent: '^todoList'
       }
     });
-
-  function TodoFooterController() {
-    var vm = this;
-  }
 
 })();
