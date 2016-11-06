8efb81131233453fb9f42f69aa6b7fd850774f2f
Alexander Holovin <dvjmixxxer@gmail.com>
2016-11-06 08:37:48 +0300

    Gulp: vendor and app js/css minify, env check function for prevent bugs

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,34 @@ var path = require('path');
 var concat = require('gulp-concat');
 var order = require("gulp-order");
 var bowerFiles = require('main-bower-files');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var gulpif = require('gulp-if');
 
+var env = {
+  production: 'production',
+  development: 'development'
+};
 
 var paths = {
-  build: './build'
+  app: './todoApp',
+  build: './build',
+
+  js: 'js',
+  css: 'css',
+
+  entryPoint: 'index.html',
+  appConcatFileJS: 'app.js',
+  appConcatFileCSS: 'app.css'
 };
 
 
-gulp.task('scripts-vendor', function() {
-  var filter = process.env.NODE_ENV === 'development' ? '**/*.js' : '**/*.min.js';
+gulp.task('default', ['clean']);
+
+gulp.task('scripts-vendor', ['set-dev-node-env'], function() {
+  checkEnv();
+
+  var filter = process.env.NODE_ENV === env.development ? '**/*.js' : '**/*.min.js';
 
   return gulp.src(bowerFiles(filter))
     .pipe(order([
@@ -24,10 +43,38 @@ gulp.task('scripts-vendor', function() {
       '**/*'
     ]))
     .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(path.join(paths.build, paths.js)));
+});
+
+gulp.task('styles-vendor', function() {
+  checkEnv();
+
+  return gulp.src(bowerFiles('**/*.css'))
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest(path.join(paths.build, paths.css)));
+});
+
+gulp.task('scripts-app', function () {
+  checkEnv();
+
+  var sources = gulp.src(path.join(paths.app, '**/*.js'));
+
+  return sources
+    .pipe(concat(paths.appConcatFileJS))
+    .pipe(uglify())
     .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('default', ['clean', 'set-prod-node-env', 'scripts-vendor']);
+gulp.task('styles-app', function () {
+  checkEnv();
+
+  var sources = gulp.src(path.join(paths.app, '**/*.css'));
+
+  return sources
+    .pipe(gulpif(process.env.NODE_ENV === env.production), cleanCSS())
+    .pipe(concat(paths.appConcatFileCSS))
+    .pipe(gulp.dest(paths.build));
+});
 
 gulp.task('clean', function () {
   del([path.join(paths.build, '/**'), path.join('!', paths.build)]).then(function (paths) {
@@ -36,9 +83,16 @@ gulp.task('clean', function () {
 });
 
 gulp.task('set-dev-node-env', function() {
-  return process.env.NODE_ENV = 'development';
+  return process.env.NODE_ENV = env.development;
 });
 
 gulp.task('set-prod-node-env', function() {
-  return process.env.NODE_ENV = 'production';
+  return process.env.NODE_ENV = env.production;
 });
+
+
+function checkEnv() {
+  if (process.env.NODE_ENV in env || !process.env.NODE_ENV) {
+    throw new Error(`Bad environment value (${process.env.NODE_ENV})`);
+  }
+}
